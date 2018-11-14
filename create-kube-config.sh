aafa6bdef4bde026d0c2230b46401eb72a9e4f85
sfgroups <6914061+grailsweb@users.noreply.github.com>
2018-11-13 22:06:43 -0500

    sample file

---
+++ create-kube-config.sh
@@ -0,0 +1,29 @@
+# your server name goes here
+server=https://localhost:8443
+# the name of the secret containing the service account token goes here
+name=default-token-sg96k
+
+ca=$(kubectl get secret/$name -o jsonpath='{.data.ca\.crt}')
+token=$(kubectl get secret/$name -o jsonpath='{.data.token}' | base64 -D)
+namespace=$(kubectl get secret/$name -o jsonpath='{.data.namespace}' | base64 -D)
+
+echo "
+apiVersion: v1
+kind: Config
+clusters:
+- name: default-cluster
+  cluster:
+    certificate-authority-data: ${ca}
+    server: ${server}
+contexts:
+- name: default-context
+  context:
+    cluster: default-cluster
+    namespace: default
+    user: default-user
+current-context: default-context
+users:
+- name: default-user
+  user:
+    token: ${token}
+" > sa.kubeconfig
